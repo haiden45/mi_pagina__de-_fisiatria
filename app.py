b5b348fa3194cf05e7e90bbccb266901943a09f4
Haiden45 <haidenperezdoc@hotmail.com>
2026-09-06 14:43:59 -0500

    Agrega robots.txt y sitemap.xml

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -31,6 +31,57 @@ def fisiatria():
 @app.route('/ondas')
 def ondas():
     return render_template('ondas.html')
+
+
+# =========================
+# SEO: robots.txt
+# =========================
+@app.route('/robots.txt')
+def robots():
+    contenido = """User-agent: *
+Allow: /
+
+Sitemap: https://mi-pagina-de-fisiatria.onrender.com/sitemap.xml
+"""
+    return Response(contenido, mimetype='text/plain')
+
+
+# =========================
+# SEO: sitemap.xml
+# =========================
+@app.route('/sitemap.xml')
+def sitemap():
+    contenido = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/</loc>
+    </url>
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/emg</loc>
+    </url>
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/conduccion</loc>
+    </url>
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/potenciales</loc>
+    </url>
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/fisiatria</loc>
+    </url>
+
+    <url>
+        <loc>https://mi-pagina-de-fisiatria.onrender.com/ondas</loc>
+    </url>
+
+</urlset>
+"""
+    return Response(contenido, mimetype='application/xml')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
